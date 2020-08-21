af905a09dec140a883713179d1c6486c9064dfe3
ShawnStephens <shawnstephens517@gmail.com>
2020-08-20 22:15:17 -0400

    //WIP
    Added Boilerplate React and go mod file

---
+++ tsec_comms/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>TSecLabs Communicator Stations</h1>
+
+      {/* Header */}
+      {/* Sidebar */}
+      {/* React-Router ->Chat Screen*/}
+    </div>
+  );
+}
+
+export default App;
